ba65b90569962475504878c6023e9340ff423286
101133651 <101133651@swin.edu.au>
2017-11-15 16:39:06 +1100

    Added Stop and Location models. An Amenity model can be represented as a 'Features' KeyValuePair.

---
+++ PTVWrapper/Models/Location.cs
@@ -0,0 +1,19 @@
+﻿namespace PTVWrapper.Models
+{
+    public class Location
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        /// <summary>
+        /// Constructor for creating a new <see cref="Location"/> object
+        /// </summary>
+        /// <param name="lt">Latitude as a </param>
+        /// <param name="ln"></param>
+        public Location(double lt, double ln)
+        {
+            Latitude = lt;
+            Longitude = ln;
+        }
+    }
+}
--- PTVWrapper/Models/Stop.cs
+++ PTVWrapper/Models/Stop.cs
@@ -5,5 +5,33 @@ namespace PTVWrapper.Models
 {
     public class Stop
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public RouteType RouteType { get; set; }
+        public Location Location { get; set; }
+        public KeyValuePair<string, bool> Features { get; set; }
+
+        /// <summary>
+        /// Constructor for creating a new <see cref="Stop"/> object
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <param name="nm">Name</param>
+        /// <param name="ty">Station Type</param>
+        /// <param name="ds">Description</param>
+        /// <param name="rt">Stop <see cref="Models.RouteType"/></param>
+        /// <param name="lc">Location as a <see cref="Models.Location"/></param>
+        /// <param name="ft">A <see cref="KeyValuePair{String, Boolean}"/> or <see langword="null"/> if not available</param>
+        public Stop(int id, string nm, string ty, string ds, RouteType rt, Location lc, KeyValuePair<string, bool> ft)
+        {
+            Id = id;
+            Name = nm;
+            Type = ty;
+            Description = ds;
+            RouteType = rt;
+            Location = lc;
+            Features = ft;
+        }
     }
 }
