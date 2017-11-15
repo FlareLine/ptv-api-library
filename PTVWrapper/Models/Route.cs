00f303af365e921cd83d73a7f24df676ae63319a
101133651 <101133651@swin.edu.au>
2017-11-15 16:01:49 +1100

    Added Route model and created RouteType class

--- PTVWrapper/Models/Route.cs
+++ PTVWrapper/Models/Route.cs
@@ -5,5 +5,24 @@ namespace PTVWrapper.Models
 {
     public class Route
     {
+        public RouteType Type { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Constructor for creating a new <see cref="Route"/> object
+        /// </summary>
+        /// <param name="ty"><see cref="RouteType"/> of <see cref="Route"/></param>
+        /// <param name="id">ID</param>
+        /// <param name="nm">Name</param>
+        /// <param name="nb">Route number (Not ID) as a <see langword="string"/></param>
+        public Route(RouteType ty, int id, string nm, string nb)
+        {
+            Type = ty;
+            Id = id;
+            Name = nm;
+            Number = nb;
+        }
     }
 }
---
+++ PTVWrapper/Models/RouteType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PTVWrapper.Models
+{
+    public class RouteType
+    {
+    }
+}
