fc0868c45f70fe226fe230710f4d71c362dca507
101133651 <101133651@swin.edu.au>
2017-11-15 16:03:38 +1100

    Added RouteType model

--- PTVWrapper/Models/RouteType.cs
+++ PTVWrapper/Models/RouteType.cs
@@ -5,5 +5,18 @@ namespace PTVWrapper.Models
 {
     public class RouteType
     {
+        public string Name { get; set; }
+        public int Type { get; set; }
+
+        /// <summary>
+        /// Constructor for creating a new <see cref="RouteType"/> object
+        /// </summary>
+        /// <param name="nm">Readable name</param>
+        /// <param name="ty">Type ID as a <see langword="string"/></param>
+        public RouteType(string nm, int ty)
+        {
+            Name = nm;
+            Type = ty;
+        }
     }
 }
