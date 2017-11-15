128ef055513e9327d6c909d0624a6c9862679f5e
101133651 <101133651@swin.edu.au>
2017-11-15 15:56:53 +1100

    Added Direction model

--- PTVWrapper/Models/Direction.cs
+++ PTVWrapper/Models/Direction.cs
@@ -5,5 +5,21 @@ namespace PTVWrapper.Models
 {
     public class Direction
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Route Route { get; set; }
+
+        /// <summary>
+        /// Constructor for creating a new <see cref="Direction"/> object
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="nm">Name</param>
+        /// <param name="rt">The <see cref="Models.Route"/> the <see cref="Direction"/> corresponds to</param>
+        public Direction(int id, string nm, Route rt)
+        {
+            Id = id;
+            Name = nm;
+            Route = rt;
+        }
     }
 }
