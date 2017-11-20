06be27b3680ac3fa1eec803fd45a39c0e42cec70
FlareLine <xom.flareline@gmail.com>
2017-11-21 09:44:21 +1100

    Added Outlet model and fixed the constructor documentation in Location object

--- PTVWrapper/Models/Location.cs
+++ PTVWrapper/Models/Location.cs
@@ -8,8 +8,8 @@
         /// <summary>
         /// Constructor for creating a new <see cref="Location"/> object
         /// </summary>
-        /// <param name="lt">Latitude as a </param>
-        /// <param name="ln"></param>
+        /// <param name="lt">Latitude as a <see langword="double"/></param>
+        /// <param name="ln">Longitude as a <see langword="double"/></param>
         public Location(double lt, double ln)
         {
             Latitude = lt;
---
+++ PTVWrapper/Models/Outlet.cs
@@ -0,0 +1,22 @@
+﻿namespace PTVWrapper.Models
+{
+    public class Outlet
+    {
+        public string Business { get; set; }
+        public Location Location { get; set; }
+        public string Suburb { get; set; }
+
+        /// <summary>
+        /// Constructor for creating a new <see cref="Outlet"/> object
+        /// </summary>
+        /// <param name="bn">Business name</param>
+        /// <param name="lc">Location</param>
+        /// <param name="sb">Suburb name</param>
+        public Outlet(string bn, Location lc, string sb)
+        {
+            Business = bn;
+            Location = lc;
+            Suburb = sb;
+        }
+    }
+}
